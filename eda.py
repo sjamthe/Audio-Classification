ef3c8528bf222dc92ce0a9ce7229c16a998040ed
Shirish Jamthe <sjamthe@yahoo.com>
2018-12-26 22:54:02 -0800

    completed models an ran both properly

--- eda.py
+++ eda.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
 from python_speech_features import mfcc, logfbank
+import librosa
 
 def plot_signals(signals):
     fig, axes = plt.subplots(nrows=2, ncols=5, sharex=False,
@@ -62,3 +63,79 @@ def plot_mfccs(mfccs):
             axes[x,y].get_yaxis().set_visible(False)
             i += 1
 
+#create signal envelope so se filter out low amplitude/noise
+#Ref: https://www.youtube.com/watch?v=mUXkj1BKYk0
+def envelope(y, rate, threshold):
+    mask = []
+    #we want a rolling window so we create series as it is easy with it
+    y = pd.Series(y).apply(np.abs)
+    y_mean = y.rolling(window=int(rate/10), min_periods=1, center=True).mean()
+    for mean in y_mean:
+        if mean > threshold:
+            mask.append(True)
+        else:
+            mask.append(False)
+            
+    return mask
+    
+def calc_fft(y, rate):
+    n = len(y)
+    freq = np.fft.rfftfreq(n, d=1/rate)
+    Y = abs(np.fft.rfft(y)/n)
+    
+    return (Y, freq)
+
+df = pd.read_csv('instruments.csv')
+df.set_index('fname',inplace=True)
+
+for f in df.index:
+    rate, signal = wavfile.read('wavfiles/' + f)
+    df.at[f, 'length'] = signal.shape[0]/rate
+
+classes = list(np.unique(df.label))
+class_dist = df.groupby(['label'])['length'].mean()
+
+fig, ax = plt.subplots()
+ax.set_title('Class Distribution', y=1.08)
+ax.pie(class_dist, labels=class_dist.index, autopct='%1.1f%%',
+       shadow=False, startangle=90)
+ax.axis('equal')
+plt.show()
+df.reset_index(inplace=True)
+
+signals = {}
+fft = {}
+fbank = {}
+mfccs = {}
+
+for c in classes:
+    wav_file = df[df.label == c].iloc[0,0]
+    signal, rate = librosa.load('wavfiles/'+wav_file,sr=44100)
+    mask = envelope(signal, rate, 0.0005) #0.0005 is experimental
+    signal = signal[mask]
+    signals[c] = signal
+    fft[c] = calc_fft(signal, rate)
+    
+    bank = logfbank(signal[:rate], rate, nfilt=26, nfft=1103).T
+    fbank[c] = bank
+    mel = mfcc(signal[:rate], rate, numcep=13, nfilt=26, nfft=1103).T
+    mfccs[c] = mel
+    
+plot_signals(signals)
+plt.show()
+
+plot_fft(fft)
+plt.show()
+
+plot_fbank(fbank)
+plt.show()
+
+plot_mfccs(mfccs)
+plt.show()
+
+if len(os.listdir('clean')) == 0:
+    for f in tqdm(df.fname):
+        signal, rate = librosa.load('wavfiles/'+f, sr=16000)
+        mask = envelope(signal, rate, 0.0005)
+        wavfile.write(filename='clean/'+f, rate=rate, data=signal[mask])
+        
